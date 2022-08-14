a654323d5c3cae3286f6a79e129bd00ff385ba9f
Subash Ranabhat <98657012+SUBASH-RANABHAT@users.noreply.github.com>
2022-08-14 10:48:49 +0545

    inital commit

---
+++ about.php
@@ -0,0 +1,81 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+    <title>About us</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    
+    <style>
+        .well
+        {
+            background: rgba(0,0,0,0.5);
+            border: none;
+        }
+		body
+		{
+			background-image: url('images/166015.webp');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+            background-size: cover;
+
+		}
+        .row{
+            align-items: center;
+        }
+        .col-md-4{
+            align-items: center;
+        }
+    </style>
+    
+    
+</head>
+
+<body>
+    <div class="container">
+      
+      
+       <img class="img-responsive" src="images/ac.png" style="width:100%; height:180px;">      
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="room.php">Room &amp; Facilities</a></li>
+                    <li><a href="reservation.php">Online Reservation</a></li>
+                    <li><a href="admin.php">Admin</a></li>
+                    <li class="active"><a href="about.php">About us</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://www.facebook.com"><img src="images/facebook.png" height="20px" width="20px"></a></li>
+                    <li><a href="http://www.instagram.com"><img src="images/instagram.png" height="20px" width="20px"></a></li>
+                    <li><a href="http://www.twitter.com"><img src="images/twitter.png" height="20px" width="20px"></a></li>   
+            </div>
+        </nav>
+    </div>
+   
+    <hr>
+    
+    <div class="row" style="color: white">
+            <div class="col-md-4 wellfix">
+              <h4><strong style="color: red">About us</strong></h4><br>
+              <br><hr>
+              <p>BookMeHotel reservations are a popular method for booking hotel rooms. Travelers can book rooms on a computer by using online security to protect their privacy and financial information and by using several online travel agents to compare prices and facilities at different hotels</p>
+              <br>
+              <p>Prior to the Internet, travelers could write, telephone the hotel directly, or use a travel agent to make a reservation. Nowadays, online travel agents have pictures of hotels and rooms, information on prices and deals, and even information on local resorts. Many also allow reviews of the traveler to be recorded with the online travel agent.</p>
+              <br>
+              <p>BookMeHotel reservations are also helpful for making last minute travel arrangements. Hotels may drop the price of a room if some rooms are still available. There are several websites that specialize in searches for deals on rooms.</p>
+              <p>BookMeHotel is No.1 online hotel booking site in Nepal.</p><br>
+
+            </div>  
+   
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   
+   <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
+   
